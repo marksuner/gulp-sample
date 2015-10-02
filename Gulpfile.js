c549da3f640abd3d2fceaf07f47018cdb92e53ad
Mark Jestoni Suner <mark_suner@hotmail.com>
2015-10-02 10:06:00 -0700

    init

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    server = require('gulp-server-livereload');
+
+// translate sass to css
+gulp.task('sass', function() {
+    gulp.src('./sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+//initialize web server with livereload
+gulp.task('webserver', function() {
+    gulp.src('dist')
+        .pipe(server({
+            livereload: true,
+            open: true
+        }));
+});
+
+//check whether a sass file has change then trigger sass task
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/*.scss', ['sass']);
+});
+
+//default task will trigger webserver, sass and sass:watch task
+//then by default it will watch any changes on the dist folder
+//so when a sass file was converted to css the livereload-server will
+//automatically reload the web because of changes in css files
+gulp.task('default',['webserver', 'sass', 'sass:watch']);
